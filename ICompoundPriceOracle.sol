c0dc5e564085d9cd3c1e2e53dfe2fae67319266c
Carlos Rwi <65631167+Carlos-RWI@users.noreply.github.com>
2020-06-08 04:47:41 +0200

    Add files via upload

---
+++ ICompoundPriceOracle.sol
@@ -0,0 +1,7 @@
+// ../src/compound/contracts/ICompoundPriceOracle.sol
+
+pragma solidity ^0.5.0;
+
+contract ICompoundPriceOracle {
+    function getUnderlyingPrice(address cToken) external view returns (uint256);
+}
